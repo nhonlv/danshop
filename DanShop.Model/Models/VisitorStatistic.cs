04f3e662bb546ef2c3f6a439fa2e2fabd79bf949
NhonLV <nhonlv.soict@gmail.com>
2020-02-15 00:58:01 +0700

    Generate Model

---
+++ DanShop.Model/Models/VisitorStatistic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DanShop.Model.Models
+{
+    [Table("VisitorStatistics")]
+    internal class VisitorStatistic
+    {
+        [Key]
+        public Guid ID { set; get; }
+
+        public DateTime VisitedDate { set; get; }
+
+        [MaxLength(50)]
+        public string IPAdress { set; get; }
+    }
+}
